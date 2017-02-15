190d494e2274c2dd1f4bf64b4e3d8d8934102cd6
Naz Ahmed <n@naz.io>
2017-02-15 12:24:41 -0500

    more timeline styling added

--- frontend/components/post/create_post.jsx
+++ frontend/components/post/create_post.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class CreatePost extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderForm = this.renderForm.bind(this);
+  }
+
+  renderForm(){
+    <form className="create-post-form">
+      <input type="text" placeholder="Whats on your mind?"></input>
+    </form>
+  }
+
   render () {
     return(
       <div className="create-post">
@@ -10,8 +21,13 @@ class CreatePost extends React.Component {
           <div className="create-post-header-item">Video</div>
         </div>
         <div className="create-post-main">
+
           <div className="create-post-img-wrapper">
             <img src="" className="user-pic-xs" />
+          </div>
+
+          <div className="create-post-body">
+            What's on your mind?
           </div>
         </div>
       </div>

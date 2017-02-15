94f06b7117169d49cd7d8b9df0c2897844fc5094
Naz Ahmed <n@naz.io>
2017-02-15 14:56:13 -0500

    correct comment and post styling

---
+++ frontend/components/comment/comment_box.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CommentBox extends React.Component {
+
+  render() {
+    return(
+      <div className="comment-box">
+
+      </div>
+    );
+  }
+}
+
+export default CommentBox;
--- frontend/components/post/post_item.jsx
+++ frontend/components/post/post_item.jsx
@@ -1,40 +1,47 @@
 import React from 'react';
+import CommentBox from '../comment/comment_box';
 
 class PostItem extends React.Component {
   render () {
     return(
-      <div className="post-item">
+      <div className="post-item-container">
+        <div className="post-item">
 
-        <div className="post-item-header">
-          <div className="post-item-img-wrapper">
-            <img src="" className="user-pic-xs" />
-          </div>
-
-          <div className="post-item-header-data">
-            <div className="post-item-user-data">
-              <span className="post-item-user-1">Bill Gates</span> > <span className="post-item-user-2">Mark Zuckerberg</span>
+          <div className="post-item-header">
+            <div className="post-item-img-wrapper">
+              <img src="" className="user-pic-xs" />
             </div>
-            <div className="post-item-time-data">
-              20 hrs
+
+            <div className="post-item-header-data">
+              <div className="post-item-user-data">
+                <span className="post-item-user-1">Bill Gates</span> > <span className="post-item-user-2">Mark Zuckerberg</span>
+              </div>
+              <div className="post-item-time-data">
+                20 hrs
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="post-item-body">
-          <p>Yo whaddup dude??</p>
-        </div>
-
-        <div className="post-item-content">
-
-        </div>
-
-        <div className="post-item-footer">
-          <div className="post-action-container">
-            <div className="post-action">Like</div>
-            <div className="post-action">Comment</div>
-            <div className="post-action">Share</div>
+          <div className="post-item-body">
+            <p>Yo whaddup dude??</p>
           </div>
+
+          <div className="post-item-content">
+
+          </div>
+
+          <div className="post-item-footer">
+            <div className="post-action-container">
+              <div className="post-action">Like</div>
+              <div className="post-action">Comment</div>
+              <div className="post-action">Share</div>
+            </div>
+          </div>
+
         </div>
+
+
+        <CommentBox />
 
       </div>
     );
